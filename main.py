4926dee2a20ee47fe4831fbeb4a9b096c6eff036
Lanika-la <masha.berlova0808@gmail.com>
2025-01-22 19:46:19 +0300

    Create main.py
    Код для подключения базы данных(не рабочий:( )

---
+++ main.py
@@ -0,0 +1,46 @@
+import os
+import sqlite3
+
+from functools import wraps
+from flask import Flask, render_template,request,current_app
+
+app = Flask(__name__)
+
+def connect_db(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        result = None
+        try:
+            db_path = os.path.join(current_app.root_path, "db", "main.db")
+            connection = sqlite3.connect(db_path)
+            connection.row_factory = sqlite3.Row
+
+            print("Успещное подклюиличь к SQLite БД.")
+
+            try:
+                cursor = connection.cursor()
+                result = func(cursor, connection, *args, **kwargs)
+            finally:
+                connection.close()
+                print("Соединение с SQLite БД закрыто.")
+
+
+        except Exception as ex:
+            print("Не удалочь подключиться к SQLite БД.")
+            print(ex)
+        return result
+    return wrapper
+
+
+
+@connect_db
+@app.route("/",endpoint="index",methods=["GET","POST"])
+def index():
+    a=5
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+    app.run(port=800, host="127.0.0.1")
+
+
