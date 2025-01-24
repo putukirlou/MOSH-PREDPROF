31514d7cd920e665c884ca2e7547b5e575d2312d
Lanika-la <masha.berlova0808@gmail.com>
2025-01-24 22:31:51 +0300

    main

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import os
 import sqlite3
 from functools import wraps
-from flask import Flask, render_template, current_app
+from flask import Flask, current_app
 
-app = Flask(__name__, static_url_path="", static_folder="static", template_folder="templates")
+from view_index import *
+from view_addatm import *
+from view_listatm import *
+from view_command import *
+from view_mechanics import *
+
+app = Flask(
+    __name__, static_url_path="", static_folder="static", template_folder="templates"
+)
 
 
 def connect_db(func):
@@ -11,11 +19,10 @@ def connect_db(func):
     def wrapper(*args, **kwargs):
         result = None
         try:
-            db_path = os.path.join(current_app.root_path, "db", "main.db")
+            db_path = os.path.join(current_app.root_path, 'db', 'main (2).db')
             connection = sqlite3.connect(db_path)
             connection.row_factory = sqlite3.Row
-
-            print("Успещное подклюиличь к SQLite БД.")
+            print("Успешно подключились к SQLite БД.")
             try:
                 cursor = connection.cursor()
                 result = func(cursor, connection, *args, **kwargs)
@@ -23,7 +30,7 @@ def connect_db(func):
                 connection.close()
                 print("Соединение с SQLite БД закрыто.")
         except Exception as ex:
-            print("Не удалочь подключиться к SQLite БД.")
+            print("Не удалось подключиться к SQLite БД.")
             print(ex)
         return result
 
@@ -32,9 +39,80 @@ def connect_db(func):
 
 @app.route("/", endpoint="index", methods=["GET", "POST"])
 @connect_db
-def index(cursor, connection):
-    return render_template("index.html")
+def index_route(cursor, connection):
+    return index(cursor, connection)
+
+
+@app.route("/addatm", endpoint="addatm", methods=["GET", "POST"])
+@connect_db
+def addatm_route(cursor, connection):
+    return addatm(cursor, connection)
+
+
+@app.route("/listatm", endpoint="listatm", methods=["GET", "POST"])
+@connect_db
+def listatm_route(cursor, connection):
+    return listatm(cursor, connection)
+
+
+@app.route("/command", endpoint="command", methods=["GET", "POST"])
+@connect_db
+def command_route(cursor, connection):
+    return command(cursor, connection)
+
+
+@app.route("/mechanics", endpoint="mechanics", methods=["GET", "POST"])
+@connect_db
+def mechanics_route(cursor, connection):
+    return mechanics(cursor, connection)
+
+@app.route("/deleteatm", endpoint="deleteatm", methods=["GET", "POST"])
+@connect_db
+def deleteatm(cursor, connection):
+    args = dict()
+    args["title"] = "Удалить банкомат"
+    id = request.args.get("id")
+    if not id:
+        args["error"] = "Номер банкомата пустой"
+        return render_template("error.html", args=args)
+
+    query = (
+        f"DELETE FROM atm WHERE id={id};"
+    )
+    cursor.execute(query)
+    connection.commit()
+
+    return redirect(f"/listatm", 301)
+
+
+@app.route("/editatm", endpoint="editatm", methods=["GET", "POST"])
+@connect_db
+def editatm(cursor, connection):
+    args = dict()
+    args["title"] = "Редактировать карту"
+    if request.method == "GET":
+        id = request.args.get("id")
+        if not id:
+            args["error"] = "Номер банкомата пустой"
+            return render_template("error.html", args=args)
+
+        args["id"] = id
+        return render_template("editatm.html", args=args)
+    elif request.method == "POST":
+        ll=request.form.get("ll", "")
+        id=request.form.get("id", "")
+        if not ll:
+            args["error"] = "Не ввели Device ID"
+            return render_template("error.html", args=args)
+        query = (
+            f"UPDATE atm SET ll = '{ll}' WHERE id={id};"
+        )
+        cursor.execute(query)
+        connection.commit()
+
+        return redirect(f"/listatm", 301)
+
 
 
 if __name__ == '__main__':
-    app.run(port=800, host="127.0.0.1")
+    app.run(port=8080, host='127.0.0.1')
